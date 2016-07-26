1ebeeadc943cd7b16f983a7b773c2f51a984b7f1
Tomas Ohlson <ohlson.tomas@gmail.com>
2016-07-26 22:06:33 +0200

    graph one done

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import {Link} from 'react-router';
 import {version} from '../../package.json';
 import SCBLineChart from './SCBLineChart';
+import Dropdown from 'react-dropdown'
 
-const App = ({children}) => {
-  return (
-    <div>
-      <header>
-        <h1>Statistics</h1>
-        <Link to="/about">About</Link>
-        <Link to="/poweredby">Powered by</Link>
-      </header>
-      <SCBLineChart url="/BE/BE0101/BE0101A/BefolkningNy"/>
-      <section>
-        {children || 'Welcome to React Starterify'}
-      </section>
-    </div>
-  )
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static propTypes = {
+    children: React.PropTypes.object
+  };
 
-App.propTypes = {children: React.PropTypes.object};
+  render() {
+    let codes = [{code: 'Region', index: [177, 1, 0]}, {code: 'ContentsCode', index: [0]}, {code: 'Tid', index: []}];
+    return (
+      <div>
+        <header>
+          <h1>Statistics</h1>
+          <Link to="/about">About</Link>
+          <Link to="/poweredby">Powered by</Link>
+        </header>
+        <SCBLineChart url="/BE/BE0101/BE0101A/BefolkningNy" codes={codes}/>
+        {/*<Dropdown options={this.options} placeholder="Select an option" />*/}
+        <section>
+          {this.props.children || 'Welcome to React Starterify'}
+        </section>
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/components/SCBLineChart.js
+++ src/components/SCBLineChart.js
@@ -1,31 +1,94 @@
 import React, {PropTypes} from 'react';
 import {Line} from "react-chartjs";
 
+class Color {
+  static interpolate(start, end, steps, count) {
+    var s = start,
+      e = end,
+      final = s + (((e - s) / steps) * count);
+    return Math.floor(final);
+  }
+
+  static getColorString(index, steps) {
+    console.log("variable ",index, steps);
+    let red = [232, 9, 26];
+    let white = [255, 255, 255];
+    let green = [6, 170, 60];
+    let start = green;
+    let end = white;
+
+    return Color.getColor(index, steps, start, end)
+  }
+
+  static getColor(index, steps, start, end) {
+    if (index > steps) {
+      start = white;
+      end = red;
+      index = index % (steps + 1);
+    }
+    var startColors = start, endColors = end;
+    var r = Color.interpolate(startColors[0], endColors[0], steps, index);
+    var g = Color.interpolate(startColors[1], endColors[1], steps, index);
+    var b = Color.interpolate(startColors[2], endColors[2], steps, index);
+
+    return "rgba(" + r + "," + g + "," + b + ",0.2)"
+  }
+}
+
 class SCBLineChart extends React.Component {
   static baseUrl = "https://api.scb.se/OV0104/v1/doris/sv/ssd";
   static propTypes = {
-    url: PropTypes.string.isRequired
+    url: PropTypes.string.isRequired,
+    codes: PropTypes.array.isRequired
+  };
+
+  rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+      case 0: r = 1; g = f; b = 0; break;
+      case 1: r = q; g = 1; b = 0; break;
+      case 2: r = 0; g = 1; b = f; break;
+      case 3: r = 0; g = q; b = 1; break;
+      case 4: r = f; g = 0; b = 1; break;
+      case 5: r = 1; g = 0; b = q; break;
+    }
+    // var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    var d = "rgba(" + r * 255 + "," + g * 255 + "," + b * 255 + ",0.2)";
+    return (d);
+  }
+
+  makeDataSets(values = {'code':[]}) {
+    const len = Object.keys(values).length;
+    return Object.keys(values).map((val, index)=>{
+      return {
+        label: val,
+        title: 'bajs2',
+        tooltipTemplate: "bajs",
+        fillColor: this.rainbow(len, index),
+        data: values[val],
+        borderWidth: 3
+      }
+    });
   };
 
   componentDidMount() {
     return this.get('get', this.props.url).then(
       res => {
-        let codes = [{code: 'Region', index: 1}, {code: 'ContentsCode', index: 1}, {code: 'Tid', index: 0}];
         console.log("variable ", res);
-        this.querySCB(this.props.url, codes, res.variables).then(res => {
+        this.querySCB(this.props.url, this.props.codes, res.variables).then(res => {
           console.log('fun ', res);
           let chartData = {
             type: 'line',
             fill: false,
-            labels: res.Tid.keys,
-            datasets: [{
-              label: 'Hejj',
-              title: 'bajs2',
-              tooltipTemplate: "bajs",
-              fillColor: "rgba(220,0,0,0.2)",
-              data: res.Region.values,
-              borderWidth: 3
-            }]
+            labels: res.Tid.keys.splice(0,res.Tid.keys.length/ Object.keys(res.Region.values).length),
+            datasets: this.makeDataSets(res.Region.values)
           };
           console.log("variable setting chartData", chartData);
           this.setState({chartData: chartData});
@@ -70,14 +133,15 @@ class SCBLineChart extends React.Component {
         let allKeys = resp.data.map(val => {
           return val.key[i]
         });
-        let allValues = resp.data.map(val => {
-          return val.values[i]
+        let allValues = {};
+        resp.data.map(val => {
+          if(!allValues[val.key[i]]) {allValues[val.key[i]] = []}
+          allValues[val.key[i]].push(val.values[i])
         });
         res[resp.columns[i].code] = {};
         res[resp.columns[i].code].keys = allKeys;
         res[resp.columns[i].code].values = allValues;
       }
-      console.log("final ", res);
       return res
     }, error => {
       console.log("err ", error)
@@ -111,9 +175,17 @@ class SCBLineChart extends React.Component {
       return val.code == code
     });
 
-    if (index) {
-      return [codeValues[0].values[index - 1]]
+    //debug
+    for(var i =0; i<codeValues[0].valueTexts.length; i++){
+      console.log(code, codeValues[0].valueTexts[i], codeValues[0].values[i], 'HAS INDEX ' + i)
     }
+
+    if (index.length) {
+      return index.map(val => {
+        return codeValues[0].values[val]
+      })
+    }
+
     return codeValues[0].values
   }
 
